337befd9fac842a2c15e582494e090585aa70e41
wengll <371541074@qq.com>
2017-09-28 17:30:54 +0800

    * 增加反射工具类和bean解析工具类

---
+++ src/main/java/com/wengll/framework/helper/BeanHelper.java
@@ -0,0 +1,41 @@
+package com.wengll.framework.helper;
+
+import com.wengll.framework.util.ReflectionUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * bean助手类
+ */
+public class BeanHelper {
+
+    private static final Map<Class<?>, Object> BEAN_MAP = new HashMap<>();
+
+    static {
+        Set<Class<?>> beanClassSet = ClassHelper.getBeanClassSet();
+        for(Class<?> cls : beanClassSet){
+            Object obj = ReflectionUtil.newInstance(cls);
+            BEAN_MAP.put(cls, obj);
+        }
+    }
+
+    /**
+     * 获取Bean映射
+     */
+    public static Map<Class<?>, Object> getBeanMap() {
+        return BEAN_MAP;
+    }
+
+    /**
+     * 获取Bean实例
+     */
+    @SuppressWarnings("unchecked")
+    public static <T> T getBean(Class<?> cls){
+        if(!BEAN_MAP.containsKey(cls)){
+            throw new RuntimeException("Can not get been by class " + cls);
+        }
+        return (T) BEAN_MAP.get(cls);
+    }
+}
--- src/main/java/com/wengll/framework/util/ReflectionUtil.java
+++ src/main/java/com/wengll/framework/util/ReflectionUtil.java
@@ -3,6 +3,9 @@ package com.wengll.framework.util;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 /**
  * 反射工具类
  */
@@ -21,5 +24,40 @@ public class ReflectionUtil {
             throw new RuntimeException(e);
         }
         return instance;
+    }
+
+    /**
+     * 调用方法
+     * @param obj
+     * @param method
+     * @param args
+     * @return
+     */
+    public static Object invokeMethod(Object obj, Method method, Object...args){
+        Object result = null;
+        try {
+            method.setAccessible(true);
+            result = method.invoke(obj, args);
+        } catch (Exception e){
+            LOGGER.error("invoke method failure", e);
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+    /**
+     * 设置成员变量的值
+     * @param obj
+     * @param field
+     * @param value
+     */
+    public static void setField(Object obj, Field field, Object value){
+        try {
+            field.setAccessible(true);
+            field.set(obj, value);
+        } catch (Exception e){
+            LOGGER.error("set field failure", e);
+            throw new RuntimeException(e);
+        }
     }
 }
